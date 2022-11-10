1f2b3d89ef0ca5cfc0a17691cf7b562c06c20db7
PeterFlandin <peterflandin@gmail.com>
2022-11-10 15:47:22 +0100

    Ajout css

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import Cards from "./Components/Cards";
 
 function App() {
   const [meals, setMeals] = useState([]);
-  console.log(meals);
+
 
   useEffect(() => {
     axios
       .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=`)
       .then((res) => setMeals(res.data.meals));
   }, []);
-
   return (
     <div className="App">
+      <div className="title">
+        <h1>Cooking Recipes</h1>
+        <div className="search">
+          <input type="text" placeholder="Search for your dish" value="" />
+          <button type="button">Search</button>
+        </div>
+         </div>
+         <div className="card-container">
       {meals.map((meal) => (
         <Cards key={meal.idmeal} meal={meal} />
+
       ))}
+      </div>
     </div>
   );
 }
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import "./Card.css"
 
 const Cards = ({meal}) => {
 
     return (
-
-        <div>
+        <div className='cards'>
             <h2>{meal.strMeal}</h2>
             <img src={meal.strMealThumb} alt={meal.strTags} />
             <h3>Origin : {meal.strArea}</h3>
